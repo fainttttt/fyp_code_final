3178c716f4b119f8032df1268bf1c474c58b1f63
fainttttt <fabiankoh14@gmail.com>
2024-11-02 18:38:01 +0800

    Added Analysis on Portfolio Weights

--- portfolio_weight_analysis.py
+++ portfolio_weight_analysis.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.patches import Rectangle
-import re
 
 os.chdir('/Users/fabian/Desktop/FYP/FYP_Code/code')
 current_dir = os.getcwd()
@@ -41,6 +39,22 @@ for key,value in portfolio_weight_obtained.items():
     portfolio_names.append(key)
 portfolio_names
 
+# find number of zero allocation
+zero_indicator = pd.DataFrame(columns = portfolio_names)
+zero_indicator.shape
+
+for ix in range(len(portfolio_names)):
+    portfolio_name = portfolio_names[ix]
+    portfolio_weight = portfolio_weight_obtained[portfolio_name]
+    bool_df = (portfolio_weight == 0).astype(int)
+    vector = np.sum(bool_df,axis = 0).values
+    zero_indicator[portfolio_name] = vector
+
+zero_indicator.index = portfolio_weight_obtained[portfolio_names[0]].columns.tolist()
+zero_indicator = zero_indicator.T
+zero_indicator['lookup'] = zero_indicator.index
+zero_indicator
+
 # find number of maxed-out allocation
 max_indicator = pd.DataFrame(columns = portfolio_names)
 max_indicator.shape
@@ -62,15 +76,76 @@ current_dir = os.getcwd()
 relative_file_path = os.path.join(current_dir, 'data_clean', 'lookup_table.csv')
 lookup_df = pd.read_csv(relative_file_path)
 
+zero_indicator = pd.merge(zero_indicator, lookup_df, on='lookup', how='inner')
+zero_indicator = zero_indicator.sort_values(by=['Method', 'Distribution', 'Risk'])
+zero_indicator.reset_index(drop = True, inplace = True)
+
 max_indicator = pd.merge(max_indicator, lookup_df, on='lookup', how='inner')
 max_indicator = max_indicator.sort_values(by=['Method', 'Distribution', 'Risk'])
 max_indicator.reset_index(drop = True, inplace = True)
 
-max_indicator['row_sum'] = max_indicator.iloc[:, :9].sum(axis=1)
-print(max_indicator)
+# number of times we hit diversification threshold across assets and methods
+zero_filtered_data = zero_indicator.iloc[:, list(range(9)) + [10]]
+zero_stats = zero_filtered_data.groupby('Method').mean()
+zero_stats
 
-my_pivot = max_indicator.pivot_table(index=['Distribution', 'Risk'], 
-                                     columns='Method', 
-                                     values='row_sum').reset_index()
-my_pivot.reset_index(drop = True, inplace = True)
-print(my_pivot)
+# number of times we hit diversification threshold across assets and methods
+max_filtered_data = max_indicator.iloc[:, list(range(9)) + [10]]
+max_stats = max_filtered_data.groupby('Method').mean()
+max_stats
+
+max_stats_transpose = max_stats.T
+zero_stats_transpose = zero_stats.T
+
+# Plotting
+plt.figure(figsize=(14, 7))
+
+# Plotting Zero Hits
+for column in zero_stats_transpose.columns:
+    plt.plot(zero_stats_transpose.index, zero_stats_transpose['mpt'], marker='o', linestyle='-', 
+             label=f'Zero Hits - MPT', color='blue')  # Zero Hits Method 1
+
+for column in zero_stats_transpose.columns:
+    plt.plot(zero_stats_transpose.index, zero_stats_transpose['proposed'], marker='x', linestyle='--', 
+             label=f'Zero Hits - Proposed', color='blue')  # Zero Hits Method 2
+    
+plt.text(zero_stats_transpose.index[-1], zero_stats_transpose['mpt'].iloc[-1], ' Zero Hits - MPT', 
+         verticalalignment='center', color='blue', fontsize=10)
+plt.text(zero_stats_transpose.index[-1], zero_stats_transpose['proposed'].iloc[-1], ' Zero Hits - Proposed', 
+         verticalalignment='center', color='blue', fontsize=10)
+
+# Plotting Threshold Hits
+for column in max_stats_transpose.columns:
+    plt.plot(max_stats_transpose.index, max_stats_transpose['mpt'], marker='s', linestyle='-', 
+             label=f'Threshold Hits - MPT', color='orange')  # Threshold Hits Method 1
+
+for column in max_stats_transpose.columns:
+    plt.plot(max_stats_transpose.index, max_stats_transpose['proposed'], marker='d', linestyle='--', 
+             label=f'Threshold Hits - Proposed', color='orange')  # Threshold Hits Method 2
+    
+plt.text(max_stats_transpose.index[-1], max_stats_transpose['mpt'].iloc[-1], ' Threshold Hits - MPT', 
+         verticalalignment='center', color='orange', fontsize=10)
+plt.text(max_stats_transpose.index[-1], max_stats_transpose['proposed'].iloc[-1], ' Threshold Hits - Proposed', 
+         verticalalignment='center', color='orange', fontsize=10)
+
+plt.title('Comparison of Mean Hits for Different Methods across Sectors')
+plt.xlabel('Sectors')
+plt.ylabel('Number of Hits')
+plt.grid(True)
+
+# Adjust x-axis labels
+plt.xticks(rotation=45, fontsize=10)  # Rotate and reduce font size
+plt.tight_layout()  # Adjust layout to prevent clipping
+plt.savefig('Analysis_of_Portfolio_Weight.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+
+# max_indicator['row_sum'] = max_indicator.iloc[:, :9].sum(axis=1)
+# print(max_indicator)
+
+# my_pivot = max_indicator.pivot_table(index=['Distribution', 'Risk'], 
+#                                      columns='Method', 
+#                                      values='row_sum').reset_index()
+# my_pivot.reset_index(drop = True, inplace = True)
+# print(my_pivot)
